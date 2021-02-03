44748a263aaad8df1044ec4020c2473c212e0463
Joe Brimeyer <jbrimeyer@gmail.com>
2021-02-02 20:05:50 -0800

    proptypes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
-const Header = () => {
+import PropTypes from "prop-types";
+
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Task Manager</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+Header.defaultProps = {
+  title: "Task Manager",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
